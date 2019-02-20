de3e3d301630a21c6ba00cdd383cfe20a7604b5e
Sergey Tovt <st@norse.co>
2019-02-20 10:33:56 +0200

    Initial commit

---
+++ models/circle.js
@@ -0,0 +1,37 @@
+{
+
+"metadata":
+{
+"sourceFile": "krug.max",
+"generatedBy": "3ds max ThreeJSExporter",
+"formatVersion": 3.1,
+"vertices": 6400,
+"normals": 12514,
+"colors": 0,
+"uvs": 2290,
+"triangles": 12514,
+"materials": 8
+},
+
+"materials": [
+{
+"DbgIndex" : 0,
+"DbgName"  : "dummy",
+"colorDiffuse"  : [1.0000, 0.0000, 0.0000],
+"vertexColors" : false
+},
+
+{
+"DbgIndex" : 1,
+"DbgName"  : "Material #28",
+"colorDiffuse"  : [0.5880, 0.5880, 0.5880],
+"colorAmbient"  : [0.5880, 0.5880, 0.5880],
+"colorSpecular"  : [0.9000, 0.9000, 0.9000],
+"transparency"  : 1.0,
+"specularCoef"  : 10.0,
+"vertexColors" : false
+},
+
+{
+"DbgIndex" : 2,
+"DbgName"  : "Material #28",
